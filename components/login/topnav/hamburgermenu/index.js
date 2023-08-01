eb5097c4500f2fd096d6664aa4a94d7d3328ed11
jjsshhhhh <jshinkor00@naver.com>
2023-08-01 15:48:00 +0900

    feat: Hamburgermenu Router 추가

--- components/login/topnav/hamburgermenu/index.js
+++ components/login/topnav/hamburgermenu/index.js
@@ -1,27 +1,54 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const HamburgerMenu = ({ bgColor }) => {
+  const router = useRouter();
 
-    return (
-        <>
-          <div className={bgColor}>
-              <div className=" mt-20 flex-col border-t-2 border-t-white">
-                  <div className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 ">
-                      메인 메뉴
-                  </div>
-                  <div className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 ">
-                      사진 업로드
-                  </div>
-                  <div className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 ">
-                      오늘 먹은 음식
-                  </div>
-                  <div className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 ">
-                      Calendar
-                  </div>
-              </div>
+  const handleMainMenuClick = () => {
+    router.push('/main');
+  };
+  const handleMoveCaptureClick = () => {
+    router.push('/capture');
+  };
+  const handleTodayEatFoodClick = () => {
+    console.log('TodayEatFood Clicked');
+  };
+  const handleCalendarClick = () => {
+    console.log('Calendar Clicked');
+  };
+
+  return (
+    <>
+      <div className={bgColor}>
+        <div className=" mt-20 flex-col border-t-2 border-t-white">
+          <div
+            className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 "
+            onClick={handleMainMenuClick}
+          >
+            메인 메뉴
           </div>
-        </>
-    );
+          <div
+            className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 "
+            onClick={handleMoveCaptureClick}
+          >
+            사진 업로드
+          </div>
+          <div
+            className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 "
+            onClick={handleTodayEatFoodClick}
+          >
+            오늘 먹은 음식
+          </div>
+          <div
+            className=" bg-yellow1 border-b-[1px] rounded-xl border-dashed border-white flex justify-center p-10 text-white active:bg-red1 "
+            onClick={handleCalendarClick}
+          >
+            Calendar
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HamburgerMenu;

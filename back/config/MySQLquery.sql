03bb6bf99cbf69508f462880c76f2e6e5ab6ff5b
jjsshhhhh <jshinkor00@naver.com>
2023-08-09 13:49:07 +0900

    docs: MySQLquery file

--- back/config/MySQLquery.sql
+++ back/config/MySQLquery.sql
@@ -19,22 +19,22 @@ CREATE TABLE userTable (
 
 CREATE TABLE todayFood (
 	email varchar(50) not null,
-    id varchar(50) not null PRIMARY KEY,
-    food_name varchar(50),
-    image varchar(300) not null,
-    date varchar(30) not null,
-    memo varchar(300),
+  id varchar(50) not null PRIMARY KEY,
+  food_name varchar(50),
+  image varchar(300) not null,
+  date varchar(30) not null,
+  memo varchar(300),
 	FOREIGN KEY(email) REFERENCES userTable(email)
 ) charset=utf8;
 
 CREATE TABLE foodNutrition (
 	id varchar(50) not null,
-    amount varchar(10),
-    natrium varchar(10),
-    protein varchar(10),
-    sugar varchar(10),
-    energy varchar(10),
-    fat varchar(10),
-    carbohydrate varchar(10),
-    FOREIGN KEY(id) REFERENCES todayFood(id)
+  amount varchar(10),
+  natrium varchar(10),
+  protein varchar(10),
+  sugar varchar(10),
+  energy varchar(10),
+  fat varchar(10),
+  carbohydrate varchar(10),
+  FOREIGN KEY(id) REFERENCES todayFood(id)
 ) charset=utf8;

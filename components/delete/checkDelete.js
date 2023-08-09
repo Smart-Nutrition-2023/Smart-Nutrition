ac0cde79a8b27a8c8c3214dcb9a7b07debbffe6e
jjsshhhhh <jshinkor00@naver.com>
2023-08-09 10:07:53 +0900

    fix: CheckDeleteModal click event

--- components/delete/checkDelete.js
+++ components/delete/checkDelete.js
@@ -29,7 +29,12 @@ const CheckDeleteModal = ({ isSetCheckDeleteModal, foodId }) => {
         }}
         className="inset-0 absolute"
       >
-        <div className="bg-white w-[300px] shadow-lg border-2 border-slate-200 px-8 py-6 mb-4 fixed rounded-xl left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div
+          className="bg-white w-[300px] shadow-lg border-2 border-slate-200 px-8 py-6 mb-4 fixed rounded-xl left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
           <div className="flex justify-center">정말 삭제하시겠습니까?</div>
 
           <div className="flex justify-center mx-5 mt-6">
@@ -39,7 +44,12 @@ const CheckDeleteModal = ({ isSetCheckDeleteModal, foodId }) => {
             >
               예
             </div>
-            <div className="rounded-2xl w-[60px] flex justify-center items-center bg-red-600 text-white">
+            <div
+              className="rounded-2xl w-[60px] flex justify-center items-center bg-red-600 text-white"
+              onClick={(e) => {
+                isSetCheckDeleteModal(false);
+              }}
+            >
               아니오
             </div>
           </div>

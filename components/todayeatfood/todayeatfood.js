97685cb486b5e44d4f3517e37dd8fe64973029b5
jjsshhhhh <jshinkor00@naver.com>
2023-08-11 16:55:56 +0900

    design: Modify slick slider css

--- components/todayeatfood/todayeatfood.js
+++ components/todayeatfood/todayeatfood.js
@@ -2,20 +2,53 @@ import Slider from 'react-slick';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import Image from 'next/image';
+import { useRef } from 'react';
+import { ArrowRightOutlined, ArrowLeftOutlined } from '@mui/icons-material';
 
-const settings = {
-  className: ' ',
-  centerMode: true,
-  infinite: true,
-  dots: true,
-  centerPadding: '30px',
-  slidesToShow: 1,
-  speed: 500,
+const Prev = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, color: 'black' }}
+      onClick={onClick}
+    >
+      <ArrowLeftOutlined fontSize="large" />
+    </div>
+  );
+};
+
+const Next = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, color: 'black' }}
+      onClick={onClick}
+    >
+      <ArrowRightOutlined fontSize="large" />
+    </div>
+  );
 };
 
 const TodayEatFood = ({ todayFoodInfo }) => {
   const router = useRouter();
   const { user } = useSelector((state) => state.user);
+
+  const slickRef = useRef(null);
+
+  const settings = {
+    centerMode: true,
+    infinite: true,
+    dots: true,
+    centerPadding: '0px', // centerPadding: '30px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplay: true,
+    prevArrow: <Prev />,
+    nextArrow: <Next />,
+  };
 
   const routeTodayFoodEatDetail = () => {
     if (user == null) {
@@ -35,15 +68,13 @@ const TodayEatFood = ({ todayFoodInfo }) => {
     }
   };
 
-  console.log('TodayFoodInfo list test', todayFoodInfo);
-
   return (
     <div className="">
-      <div className="font-bold px-8 pt-6 pb-2 mt-6 text-xl animate-pulse  ">
+      <div className="font-bold px-8 pt-6 pb-2 mt-6 text-xl animate-pulse">
         <p>오늘 먹은 음식</p>
       </div>
       <div className="px-8 ml-5 mr-5 rounded-2xl">
-        <Slider {...settings}>
+        <Slider {...settings} ref={slickRef}>
           {todayFoodInfo &&
             todayFoodInfo.map((images) => (
               <div
@@ -53,21 +84,20 @@ const TodayEatFood = ({ todayFoodInfo }) => {
                   routeTodayFoodEatDetail();
                 }}
               >
-                <div className="absolute h-[175px] overflow-hidden">
-                  <img
-                    className="w-100 h-100 rounded-t-3xl px-2"
-                    layout="fill"
-                    src={`http://localhost:5000/${images.image}`}
-                  ></img>
-                </div>
+                <Image
+                  className="rounded-3xl"
+                  src={`http://localhost:5000/${images.image}`}
+                  layout="fill"
+                  objectFit="cover"
+                />
 
-                <div className="w-full absolute px-2 top-[175px]">
-                  <div className="w-100 h-[60px] px-4 bg-main rounded-b-3xl break-all">
-                    <p className="font-bold text-lg pt-1">{images.food_name}</p>
-                    <p className="text-md h-1/2 break-all w-full overflow-y-hidden">
-                      {images.memo}
-                    </p>
-                  </div>
+                <div className="absolute w-full bottom-0 px-6 bg-yellow1 rounded-b-3xl bg-opacity-90">
+                  <p className="text-md font-bold font-sans mt-1 mb-1 ml-[-2px] break-all">
+                    {images.food_name}
+                  </p>
+                  <p className="mb-2 flex justify-first text-xs font-sans overflow-hidden break-all h-[16px]">
+                    {images.memo}
+                  </p>
                 </div>
               </div>
             ))}

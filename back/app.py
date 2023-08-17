f515258e8ec379fa24536af642d6081f7c15cc7c
SimSangWoo <tlatkd@knu.ac.kr>
2023-08-17 13:04:07 +0900

    fix: DB modified

--- back/app.py
+++ back/app.py
@@ -1,69 +1,68 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from flask_restx import Api, Resource
 import torch
 from PIL import Image
 import json
 import pandas as pd
-# from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)
 api = Api(app)
 app.config['DEBUG'] = True
 
 # 커스텀 학습 모델
 pt_file_path = 'best.pt'
 model = torch.hub.load('./Ultralytics/yolov5', 'custom', pt_file_path, force_reload=True, source='local', trust_repo=True)
+xl = pd.read_excel('db.xlsx', sheet_name="Sheet1")
 
 @api.route('/foodimage', methods = ['POST'])
 class ObjectDetection(Resource):
 
     # POST 요청
     def post(self):
-        print('post')
-        # 이미지 직접
         img = Image.open(request.files['image'])
+        results = model(img.convert('RGB'))
 
-        results = model(img)
-        results.show()
+        return make_json(results)
 
-        # 탐지된 객체들에 대한 정보 json 형태로 저장
-        results.pandas().xyxy[0].to_json('detected.json', orient='values')
+def add_headers(j):
+    j = make_response(j)
+    j.headers.add("Access-Control-Allow-Origin", "*")
+    j.headers.add("Access-Control-Allow-Headers", "*")
+    j.headers.add("Access-Control-Allow-Methods", "*")
 
-        # confidence가 가장 높은 객체만 전달
-        with open('detected.json', mode='r+', encoding='utf8') as j:
-            json_data = json.load(j)
+    return j
 
-           # 탐지된 객체 없을 경우 -1 반환
-            if len(json_data) == 0:
-                with open('nofood.json', mode='r+', encoding='utf8') as n:
-                    nofood = json.load(n)
-                nofood = make_response(nofood)
-                nofood.headers.add("Access-Control-Allow-Origin", "*")
-                nofood.headers.add("Access-Control-Allow-Headers", "*")
-                nofood.headers.add("Access-Control-Allow-Methods", "*")
-                return nofood
+def make_json(res):
+    res.pandas().xyxy[0].to_json('detected.json', orient='values')
 
-            max_conf = 0
-            max_conf_idx = 0
-            for i in range(len(json_data)):
-                if json_data[i][4] > max_conf:
-                    max_conf = json_data[i][4]
-                    max_conf_idx = i
+    with open('detected.json', mode='r+', encoding='utf8') as j:
+        json_data = json.load(j)
 
-        xl = pd.read_excel('db1.xlsx', sheet_name="Sheet1")
-        xl = xl.loc[json_data[max_conf_idx][5]]
-        xl.drop('클래스명', inplace=True)
-        xl.to_json('food_info.json', orient='columns', force_ascii=False)
+    # 탐지된 객체 없을 경우
+    if len(json_data) == 0:
+        with open('nofood.json', mode='r+', encoding='utf8') as n:
+            nofood = json.load(n)
 
-        with open('food_info.json', mode='r+', encoding='utf8') as d:
-            detected_data = json.load(d)
-        detected_data = make_response(detected_data)
-        detected_data.headers.add("Access-Control-Allow-Origin", "*")
-        detected_data.headers.add("Access-Control-Allow-Headers", "*")
-        detected_data.headers.add("Access-Control-Allow-Methods", "*")
+        return add_headers(nofood)
 
-        return detected_data
+    # confidence가 가장 높은 객체만 전달
+    max_conf = 0
+    max_conf_idx = 0
+    for i in range(len(json_data)):
+        if json_data[i][4] > max_conf:
+            max_conf = json_data[i][4]
+            max_conf_idx = i
+
+    row_selected = xl.loc[json_data[max_conf_idx][5]].copy()
+    row_selected.drop('클래스명', inplace=True)
+    row_selected.drop('Code Name', inplace=True)
+    row_selected.to_json('food_info.json', orient='columns', force_ascii=False)
+
+    with open('food_info.json', mode='r+', encoding='utf8') as d:
+        detected_data = json.load(d)
+
+    return add_headers(detected_data)
 
 if __name__ == '__main__':
+
     app.run(port=5001)

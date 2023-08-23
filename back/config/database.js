dc09b13e905f16eaa44d8a9a6cf5d9e826d21c39
jjsshhhhh <108526743+jjsshhhhh@users.noreply.github.com>
2023-08-23 10:22:19 +0900

    docs: DB

--- back/config/database.js
+++ back/config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 module.exports = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: '3306',
   user: 'testid',
   password: 'test01!',
